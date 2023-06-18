49b8540b239822383ac1d1140677e41849995461
Gavin <gavintranquilino@gmail.com>
2023-06-18 18:33:05 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+// Require the neccessary discord.js classes
+const { Client, Events, GatewayIntentBits } = require('discord.js');
+const { token } = require('./config.json');
+
+// Create a new client instance
+const client = new  Client({ intents: [GatewayIntentBits.Guilds] });
+
+// When the client is ready, run this code (only once)
+// We use 'c' for the event parameter to keep it separate from the already defined 'client'
+client.once(Events.ClientReady, c => {
+    console.log(`Ready! Logged in as ${c.user.tag}`);
+});
+
+client.login(token);
